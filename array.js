3d9a229f89da1df7405140a3e68e202ffd269a89
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-07 10:01:32 -0500

    cleaner array merge

--- array.js
+++ array.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
 const memory = require('./memory')
 
-const testMemory = new memory
+const testMemory = new memory()
 
 class Array {
   constructor() {
@@ -13,7 +13,7 @@ class Array {
   // resize in memory
   _resize(mem) {
     const oldPtr = this.ptr
-    this.ptr = testMemory.allocate(mem) 
+    this.ptr = testMemory.allocate(mem)
     if (this.ptr === null) {
       throw new Error('Out of memory')
     }
@@ -30,7 +30,7 @@ class Array {
   //   this._resize(this.length + 1)
   //   testMemory.set(this.ptr + this.length, value)
   //   this.length++
-  // } 
+  // }
 
   // push -- O(1) best/avg, O(n) worst
   push(value) {
@@ -43,88 +43,91 @@ class Array {
 
   get(index) {
     if (index < 0 || index >= this.length) {
-      throw new Error('Index error');
+      throw new Error('Index error')
     }
-    return testMemory.get(this.ptr + index);
+    return testMemory.get(this.ptr + index)
   }
 
   pop() {
     if (this.length - 1 < 0) {
-      throw new Error('Index error');
+      throw new Error('Index error')
     }
-    const value = testMemory.get(this.ptr + this.length - 1);
-    this.length--;
-    return value;
+    const value = testMemory.get(this.ptr + this.length - 1)
+    this.length--
+    return value
   }
 
   insert(index, value) {
-    if (index < 0 || index > this.length){
-      throw new Error('Index error');
+    if (index < 0 || index > this.length) {
+      throw new Error('Index error')
     }
     if (this.length >= this._capacity) {
-      this._resize((this.length + 1) * Array.SIZE_RATIO);
+      this._resize((this.length + 1) * Array.SIZE_RATIO)
     }
-    testMemory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-    testMemory.set(this.ptr + index, value);
-    this.length++;
+    testMemory.copy(this.ptr + index + 1, this.ptr + index, this.length - index)
+    testMemory.set(this.ptr + index, value)
+    this.length++
   }
 
   remove(index) {
-    if(index < 0 || index > this.length){
-      throw new Error('Index error');
+    if (index < 0 || index > this.length) {
+      throw new Error('Index error')
     }
-    testMemory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
-    this.length--;
+    testMemory.copy(
+      this.ptr + index,
+      this.ptr + index + 1,
+      this.length - index - 1
+    )
+    this.length--
   }
 }
 
-module.exports = Array;
+module.exports = Array
 
 function main() {
-  Array.SIZE_RATIO = 3;
+  Array.SIZE_RATIO = 3
 
-  let newArray = new Array;
-  
-  newArray.push(3);
+  let newArray = new Array()
+
+  newArray.push(3)
   // Array { length: 1, _capacity: 3, ptr: 0 }
 
-  newArray.push(5);
-  newArray.push(15);
-  newArray.push(19);
-  newArray.push(45);
-  newArray.push(10);
+  newArray.push(5)
+  newArray.push(15)
+  newArray.push(19)
+  newArray.push(45)
+  newArray.push(10)
 
   //#2 Array { length: 6, _capacity: 12, ptr: 3 }
   //After the third push the length exceeds capacity then allocates the length * size ratio + length and moves pointer to 3
 
-  newArray.pop();
-  newArray.pop();
-  newArray.pop();
+  newArray.pop()
+  newArray.pop()
+  newArray.pop()
 
   //Removes the last item of the array decreasing the length three times, O(1) operation
 
   //#4
   // console.log(testMemory.get(3));
 
-  newArray.pop();
-  newArray.pop();
-  newArray.pop();
+  newArray.pop()
+  newArray.pop()
+  newArray.pop()
 
-  newArray.push('tauhida');
+  newArray.push('tauhida')
   // console.log(testMemory.get(3));
   //Inputting a string returns NaN because the Array class is defined as type 64-bit float values
   //The resize is to accomodate adding values to an array where length exceeds capacity by reallocating the array
 }
 
-main();
-
+main()
 
 // #5 URLify a string
 
-// A common mistake users make when they type in an URL is to put spaces between words or letters. 
-// A solution that developers can use to solve this problem is to replace any spaces with a %20. 
-// Write a method that takes in a string and replaces all its empty spaces with a %20. 
-// Your algorithm can only make 1 pass through the string. 
+// A common mistake users make when they type in an URL is to put spaces between words or letters.
+// A solution that developers can use to solve this problem is to replace any spaces with a %20.
+// Write a method that takes in a string and replaces all its empty spaces with a %20.
+// Your algorithm can only make 1 pass through the string.
 // Examples of input and output for this problem can be
 
 // Input: tauhida parveen
@@ -134,8 +137,8 @@ main();
 
 function URLify(string) {
   let URL = ''
-  for(let i = 0; i < string.length; i++) {
-    if(string[i] === ' ') {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
       URL += '%20'
     } else {
       URL += string[i]
@@ -146,21 +149,21 @@ function URLify(string) {
 
 // console.log(URLify('tauhida parveen'))
 // Output: tauhida%20parveen
-  
+
 // console.log(URLify('www.thinkful.com /tauh ida parv een'))
 // Output: www.thinkful.com%20/tauh%20ida%20parv%20een
 
 // #6 Filtering an array
-// Imagine you have an array of numbers. 
-// Write an algorithm to remove all numbers less than 5 from the array. 
+// Imagine you have an array of numbers.
+// Write an algorithm to remove all numbers less than 5 from the array.
 // DO NOT use Array's built-in .filter() method here; write the algorithm from scratch.
 
 let numArray = [1, 4, 5, 7, 9]
 
 function moreOrFive(array) {
   let filteredArray = []
-  for(let i = 0; i < array.length; i++) {
-    if(array[i] > 5) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 5) {
       filteredArray.push(array[i])
     }
   }
@@ -186,10 +189,10 @@ function moreOrFive(array) {
 //       currentMax = arraySum;
 //       console.log(currentMax);
 //     }
-  
+
 //     // console.log(array.slice(1));
 //     // console.log(array.slice(0, array.length - 1));
-    
+
 //     getMax(array.slice(1), currentMax);
 //     getMax(array.slice(0, array.length - 1), currentMax);
 //     getMax(array.slice(1, array.length - 1), currentMax);
@@ -199,42 +202,37 @@ function moreOrFive(array) {
 // console.log(getMax([4, 6, -3, 5, -2, 1]));
 
 function simpleGetMax(array) {
-  let currentMax = 0;
-  let finalMax = 0;
+  let currentMax = 0
+  let finalMax = 0
 
   for (let i = 0; i < array.length; i++) {
-    currentMax = Math.max(0, currentMax + array[i]);
-    finalMax = Math.max(currentMax, finalMax);
+    currentMax = Math.max(0, currentMax + array[i])
+    finalMax = Math.max(currentMax, finalMax)
   }
 
-  return finalMax;
+  return finalMax
 }
 
 // console.log(simpleGetMax([4, 6, -3, 5, -2, 1]));
 
-function mergeArrays(array1, array2){
-  let sortedMerge = [];
-  let idx1 = 0;
-  let idx2 = 0;
+let array1 = [1, 3, 4, 5]
+let array2 = [2, 2, 5, 9, 11]
 
-  while(sortedMerge.length < array1.length + array2.length){
-    if(array1[idx1] <= array2[idx2]){
-      sortedMerge.push(array1[idx1]);
-      idx1++;
-    }
-    else if(array2[idx2] <= array1[idx1]){
-      sortedMerge.push(array2[idx2]);
-      idx2++;
-    }
-    else if(array1[idx1] > sortedMerge[sortedMerge.length - 1]){
-      sortedMerge.push(array1[idx1]);
-    }
-    else if(array2[idx2] > sortedMerge[sortedMerge.length - 1]){
-      sortedMerge.push(array1[idx1]);
+function mergeArr(arr1, arr2) {
+  let idx1 = arr1.length - 1
+  let idx2 = arr2.length - 1
+  let mergedArr = []
+
+  while((arr1.length + arr2.length) > mergedArr.length) {
+    if(arr1[idx1] > arr2[idx2]) {
+      mergedArr.unshift(arr1[idx1])
+      idx1--
+    } else {
+      mergedArr.unshift(arr2[idx2])
+      idx2--
     }
   }
-
-  return sortedMerge;
+  return mergedArr
 }
 
-console.log(mergeArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+console.log(mergeArr(array2,array1))
